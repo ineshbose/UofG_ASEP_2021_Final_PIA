29ef755b234e49ab9f498f92f8a06826687c1624
DavideGrecoGit <dg.davidegreco@gmail.com>
2022-02-02 13:08:30 +0000

    Added tests

--- calapp/tests.py
+++ calapp/tests.py
@@ -1,7 +1,6 @@
 import json
 from django.test import TestCase
 
-
 class CalappTests(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
@@ -23,6 +22,39 @@ class CalappTests(TestCase):
         response = self.client.post(
             "/exponent/",
             json.dumps({"num1": 3, "num2": 2}),
+            content_type="application/json",
+        )
+        self.assertNotEqual(response.status_code, 404)
+        self.assertJSONNotEqual(response.content, {"result": 100})
+
+    def test_correct_addition(self):
+        response = self.client.post(
+            "/addition/",
+            json.dumps({"num1": 0, "num2": 6}),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.content)
+        self.assertIn("result", data)
+        self.assertEqual(data["result"], 6)
+
+    def test_negative_addition(self):
+        response = self.client.post(
+            "/addition/",
+            json.dumps({"num1": 2, "num2": -6}),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.content)
+        self.assertIn("result", data)
+        self.assertEqual(data["result"], -4)
+
+    def test_incorrect_addition(self):
+        response = self.client.post(
+            "/addition/",
+            json.dumps({"num1": 12, "num2": 2}),
             content_type="application/json",
         )
         self.assertNotEqual(response.status_code, 404)
